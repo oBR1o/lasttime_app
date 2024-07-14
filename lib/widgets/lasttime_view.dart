22230f7a14bdb94237b6aa01aa25123b5e97c899
Natthawut Suksai <owen55660@gmail.com>
2024-07-14 18:02:58 +0700

    add lasttime_viewlist

--- lib/widgets/lasttime_view.dart
+++ lib/widgets/lasttime_view.dart
@@ -1,69 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lasttime/bloc/app_bloc.dart';
+import 'package:lasttime/widgets/lasttime_viewlist.dart';
 
 class LastTimeView extends StatelessWidget {
   const LastTimeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final items = context.select((LastTimeBloc bloc) => bloc.state.items);
-    return Stack(
-      children: [
-        LimitedBox(
-          child: items.isEmpty
-              ? const SizedBox(
-                  height: 200,
-                  width: double.infinity,
-                  child: Text(
-                    "Nothing to show",
-                    textAlign: TextAlign.center,
-                  ),
-                )
-              : ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Card(
-                          color: const Color.fromARGB(255, 218, 192, 163),
-                          elevation: 4,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            side: const BorderSide(
-                                color: Color.fromARGB(255, 16, 44, 87),
-                                width: 1),
-                          ),
-                          margin: const EdgeInsets.only(
-                            left: 25,
-                            right: 25,
-                            top: 5,
-                            bottom: 5,
-                          ),
-                          child: ListTile(
-                            title: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(items[index].name),
-                                Text(
-                                  '${items[index].cycleDays.toString()} day left',
-                                ),
-                              ],
-                            ),
-                            subtitle: Text(
-                              items[index].lastAction?.toString() ?? 'No Time Stamp',
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-        ),
-      ],
-    );
+    return BlocBuilder<LastTimeBloc,LastTimeState>(
+      builder: (context, state){
+        return state is LoadingState ? const Center(
+          child: CircularProgressIndicator(),
+          ) : const LastTimeViewList();
+      });
   }
 }
---
+++ lib/widgets/lasttime_viewlist.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lasttime/bloc/app_bloc.dart';
+
+class LastTimeViewList extends StatelessWidget {
+  const LastTimeViewList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final items = context.select((LastTimeBloc bloc) => bloc.state.items);
+    return Stack(
+      children: [
+        LimitedBox(
+          child: items.isEmpty
+              ? const SizedBox(
+                  height: 200,
+                  width: double.infinity,
+                  child: Text(
+                    "Nothing to show",
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Card(
+                          color: const Color.fromARGB(255, 218, 192, 163),
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: const BorderSide(
+                                color: Color.fromARGB(255, 16, 44, 87),
+                                width: 1),
+                          ),
+                          margin: const EdgeInsets.only(
+                            left: 25,
+                            right: 25,
+                            top: 5,
+                            bottom: 5,
+                          ),
+                          child: ListTile(
+                            title: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(items[index].name),
+                                Text(
+                                  '${items[index].cycleDays.toString()} day left',
+                                ),
+                              ],
+                            ),
+                            subtitle: Text(
+                              items[index].lastAction?.toString() ?? 'No Time Stamp',
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+        ),
+      ],
+    );
+  }
+}
